4ef27bc584b9a795cd58f6060b1060522ddf0fca
Ying-yz <y264596786@outlook.com>
2025-12-26 22:26:24 +0800

    添加了菜品的分页查询业务

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,8 +1,11 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -12,4 +15,6 @@ public interface DishMapper {
     Integer countByCategoryId(Long id);
     @AutoFill(value = OperationType.INSERT)
     void insert(Dish dish);
+
+    Page<DishVO> page(DishPageQueryDTO dishPageQueryDTO);
 }
